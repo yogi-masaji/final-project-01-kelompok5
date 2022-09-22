32ea2e7601678aa4939b92852d7565d450565e74
JunarisAlf <junaris0715@gmail.com>
2022-09-22 21:52:43 +0700

    Add migration

---
+++ migrations/20220922144145-create-reflection.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reflections', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      succes: {
+        type: Sequelize.STRING
+      },
+      low_point: {
+        type: Sequelize.STRING
+      },
+      take_away: {
+        type: Sequelize.STRING
+      },
+      owner_id: {
+        type: Sequelize.STRING
+      },
+      created_date: {
+        type: Sequelize.DATE
+      },
+      modified_date: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reflections');
+  }
+};
